f32d7dbdbda1a1a2004e2afdaf0d9c8473b16603
Aziz0307 <Aziz03072018@gmail.com>
2024-11-22 19:30:49 +0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Boolean8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vvedite dva tselikh chisel!");
+            Console.Write("A= ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("B= ");
+            int B = Convert.ToInt32(Console.ReadLine());
+            bool result = A % 2 !=0 && B % 2 !=0 ? true : false;
+            Console.WriteLine($"Kazhdoe iz chisel {A} i {B} nechetnoe = {result}"); 
+        }
+    }
+}
